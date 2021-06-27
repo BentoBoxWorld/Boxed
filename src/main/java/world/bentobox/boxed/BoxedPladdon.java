3077329848c192ea3ab4f8e9b884aa24de5b22d5
tastybento <tastybento@wasteofplastic.com>
2021-06-27 09:13:36 -0700

    Convert to Pladdon

---
+++ src/main/java/world/bentobox/boxed/BoxedPladdon.java
@@ -0,0 +1,12 @@
+package world.bentobox.boxed;
+
+import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.addons.Pladdon;
+
+public class BoxedPladdon extends Pladdon {
+
+    @Override
+    public Addon getAddon() {
+        return new Boxed();
+    }
+}
